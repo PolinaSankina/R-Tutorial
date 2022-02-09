fa7a6e499c4bedda468b35060d55e65ac316a608
PolinaSankina <polina.sankina28@gmail.com>
2022-02-09 22:28:36 +0100

    initial

---
+++ Tutorial/mvBGgraph.R
@@ -0,0 +1,12 @@
+data$Treatment<- as.factor(data$Treatment)
+ggplot(data, aes(x=Time, y=BG, color=Treatment))+ geom_line()
+View(data)
+ggplot(data, aes(x=Time, y=BG, color=Treatment))+
+  geom_point(lwd=2)+
+  theme_classic()+
+  theme(  
+    axis.title=element_text(size=20,face="bold"),
+    axis.text=element_text(size=14, color="black"),
+    legend.text = element_text(size=14, color="black"),
+    legend.title = element_text(size = 14, face="bold")
+  )
